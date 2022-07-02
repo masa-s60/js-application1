2910d78a48ce358d6173f1e939e8827d1408f174
masatoshi kusano <uppleate@cordiary.net>
2022-07-02 20:47:16 +0900

    新規作成

---
+++ js-application1.js
@@ -0,0 +1,42 @@
+const fizzValue = document.getElementById('fizz');
+const buzzValue = document.getElementById('buzz');
+const button = document.getElementById('button');
+const integer = /^[1-9]|0[1-9]|[1-9][0-9]$/;
+const outputParent = document.getElementById('div');
+const outputChild = document.createElement('p');
+let result = '';
+
+const reset = () => {
+  outputChild.remove();
+  result = '';
+}
+
+const calc = (fizz, buzz) => {
+  for (let i = 1; i <= 99; i++) {
+    if((i % fizz === 0) && (i % buzz === 0)) {
+      result = result.concat(`FizzBuzz ${i}<br>`);
+    } else {
+      if(i % fizzValue.value === 0) {
+        result = result.concat(`Fizz ${i}<br>`);
+      } else {
+        if(i % buzzValue.value === 0) {
+          result = result.concat(`Buzz ${i}<br>`);
+        }
+      }
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+button.addEventListener('click', () => {
+  reset();
+  if((integer.test(fizzValue.value) === true) && (integer.test(buzzValue.value) === true)) {
+    outputChild.innerHTML = calc(fizzValue.value, buzzValue.value);
+    outputParent.appendChild(outputChild);
+  } else {
+    outputChild.innerHTML = '整数値を入力してください';
+    outputParent.appendChild(outputChild);
+  }
+});
+
