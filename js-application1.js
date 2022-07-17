73c2802355956dea92ea1d19a93f784dea21961c
masatoshi kusano <uppleate@cordiary.net>
2022-07-17 13:50:56 +0900

    関数の追加、及びHTMLの結果表示タグを修正

--- js-application1.js
+++ js-application1.js
@@ -1,37 +1,43 @@
 const fizzValue = document.getElementById('fizz');
 const buzzValue = document.getElementById('buzz');
 const button = document.getElementById('button');
-const integer = /^[1-9]|0[1-9]|[1-9][0-9]$/;
-const parent = document.getElementById('div');
+const integer = /^(?!.*\D)(?=.*[1-9]{1,2})(?!.*00).*$/;
+const parent = document.getElementById('ul');
 
 const reset = () => {
     parent.innerHTML = '';
 }
 
+const createElement = (key, num) => {
+  if(key === 1) {
+    const child = document.createElement('li');
+    child.textContent = `${num}BuzzFizz`;
+    parent.appendChild(child);
+  } else if(key === 2) {
+    const child = document.createElement('li');
+    child.textContent = `${num}Fizz`;
+    parent.appendChild(child);
+  } else if(key === 3) {
+    const child = document.createElement('li');
+    child.textContent = `${num}Buzz`;
+    parent.appendChild(child);
+  }
+}
+
 const outputDisplay = (fizz, buzz) => {
   for (let i = 1; i <= 99; i++) {
     if((i % fizz === 0) && (i % buzz === 0)) {
-      const child = document.createElement('p');
-      child.textContent = `${i}BuzzFizz`;
-      parent.appendChild(child);
-    } else {
-      if(i % fizzValue.value === 0) {
-        const child = document.createElement('p');
-        child.textContent = `${i}Fizz`;
-        parent.appendChild(child);
-      } else {
-        if(i % buzzValue.value === 0) {
-          const child = document.createElement('p');
-          child.textContent = `${i}Buzz`;
-          parent.appendChild(child);
-        }
-      }
+      createElement(1, i);
+    } else if(i % fizzValue.value === 0) {
+      createElement(2, i);
+    } else if(i % buzzValue.value === 0) {
+      createElement(3, i);
     }
   }
 }
 
 const reDoDisplay = () => {
-  const child = document.createElement('p');
+  const child = document.createElement('li');
   child.textContent = '整数値を入力してください';
   parent.appendChild(child);
 }
